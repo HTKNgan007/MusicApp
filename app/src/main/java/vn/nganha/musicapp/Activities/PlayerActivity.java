ced2c40e098281f200c60d174c212bf501cd4d6f
HTKNgan007 <nganha2004nh@gmail.com>
2024-12-22 10:01:34 +0700

    them hieu ung media_playing

--- app/src/main/java/vn/nganha/musicapp/Activities/PlayerActivity.java
+++ app/src/main/java/vn/nganha/musicapp/Activities/PlayerActivity.java
@@ -1,14 +1,17 @@
 package vn.nganha.musicapp.Activities;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.media3.common.Player;
 import androidx.media3.exoplayer.ExoPlayer;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.bumptech.glide.request.RequestOptions;
 
+import vn.nganha.musicapp.R;
 import vn.nganha.musicapp.databinding.ActivityPlayerBinding;
 import vn.nganha.musicapp.models.SongModel;
 
@@ -16,6 +19,14 @@ public class PlayerActivity extends AppCompatActivity {
 
     private ActivityPlayerBinding binding;
     private ExoPlayer exoPlayer;
+
+    Player.Listener playerListener = new Player.Listener() {
+        @Override
+        public void onIsPlayingChanged(boolean isPlaying) {
+            Player.Listener.super.onIsPlayingChanged(isPlaying); // Gọi hàm `super`
+            showGif(isPlaying); // Gọi hàm showGif
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,11 +43,22 @@ public class PlayerActivity extends AppCompatActivity {
                     .load(currentSong.getCoverUrl()) // Tải ảnh từ URL
                     .circleCrop()
                     .into(binding.songCoverImageView); // Đặt vào ImageView
-
+            Glide.with(binding.songGifImageView.getContext())
+                    .load(R.drawable.media_playing)
+                    .circleCrop()
+                    .into(binding.songGifImageView);
             // Lấy instance của ExoPlayer từ MyExoplayer
-            ExoPlayer exoPlayer = MyExoplayer.getInstance();
+            exoPlayer = MyExoplayer.getInstance();
             // Gán exoPlayer vào PlayerView
             binding.playerView.setPlayer(exoPlayer);
+            // Lắng nghe sự kiện để hiện gif
+            exoPlayer.addListener(playerListener);
         }
+    }
+
+    public void showGif(boolean show){
+        if(show)
+            binding.songGifImageView.setVisibility(View.VISIBLE);
+        else binding.songGifImageView.setVisibility(View.INVISIBLE);
     }
 }
