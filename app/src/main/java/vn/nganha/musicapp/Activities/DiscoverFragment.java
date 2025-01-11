5a51baf45644cb0b621f1fcbd792f651cef80a40
HTKNgan007 <nganha2004nh@gmail.com>
2025-01-11 10:19:47 +0700

    thêm các section và hiệu ứng thu nhỏ ở main

--- app/src/main/java/vn/nganha/musicapp/Activities/DiscoverFragment.java
+++ app/src/main/java/vn/nganha/musicapp/Activities/DiscoverFragment.java
@@ -9,20 +9,27 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.bumptech.glide.request.RequestOptions;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.List;
+import java.util.Objects;
 
 import vn.nganha.musicapp.R;
 import vn.nganha.musicapp.adapter.CategoryAdapter;
 import vn.nganha.musicapp.adapter.SectionSongListAdapter;
 import vn.nganha.musicapp.models.CategoryModel;
+import vn.nganha.musicapp.models.SongModel;
 
 public class DiscoverFragment extends Fragment {
     private RecyclerView categoriesRecyclerView;
@@ -50,7 +57,18 @@ public class DiscoverFragment extends Fragment {
         // Gọi hàm để tải dữ liệu từ Firestore
         getCategories();
         // Hiển thị thêm Section
-        setupSection(view);
+        setupSection("section_1",
+                view.findViewById(R.id.section_1_main_layout),
+                view.findViewById(R.id.section_1_title),
+                view.findViewById(R.id.section_1_recycler_view));
+        setupSection("section_2",
+                view.findViewById(R.id.section_2_main_layout),
+                view.findViewById(R.id.section_2_title),
+                view.findViewById(R.id.section_2_recycler_view));
+        setupSection("Section_3",
+                view.findViewById(R.id.section_3_main_layout),
+                view.findViewById(R.id.section_3_title),
+                view.findViewById(R.id.section_3_recycler_view));
     }
 
     private void getCategories() {
@@ -71,36 +89,35 @@ public class DiscoverFragment extends Fragment {
                 });
     }
 
-    private void setupSection(View view) {
-        // Kết nối TextView và RecyclerView của Section
-        TextView sectionTitle = view.findViewById(R.id.section_1_title); // Kết nối TextView
-        RecyclerView sectionRecyclerView = view.findViewById(R.id.section_1_recycler_view);
+    private void setupSection(String id, RelativeLayout mainLayout, TextView titleView, RecyclerView recyclerView) {
 
         // Thiết lập layout manager cho RecyclerView
-        sectionRecyclerView.setLayoutManager(
+        recyclerView.setLayoutManager(
                 new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false)
         );
 
         // Lấy dữ liệu từ Firestore
         FirebaseFirestore.getInstance()
-                .collection("sections") // Truy cập collection "sections"
-                .document("section_1") // Truy cập document "section_1"
+                .collection("sections") // Truy cập collection "sections" ở cloud firestore
+                .document(id) // Truy cập document với id đưa vào
                 .get()
                 .addOnSuccessListener(documentSnapshot -> {
                     // Chuyển đổi dữ liệu Firestore thành đối tượng CategoryModel
                     CategoryModel section = documentSnapshot.toObject(CategoryModel.class);
                     if (section != null) {
                         // Thiết lập tiêu đề (name) cho section
-                        sectionTitle.setText(section.getName()); // Gán tên section vào TextView
+                        titleView.setText(section.getName()); // Gán tên section vào TextView
 
                         // Khởi tạo adapter cho Section
                         SectionSongListAdapter sectionAdapter = new SectionSongListAdapter(section.getSongs());
 
                         // Gán adapter vào RecyclerView
-                        sectionRecyclerView.setAdapter(sectionAdapter);
+                        recyclerView.setAdapter(sectionAdapter);
 
+                        // set trạng thái hiển thị cho section nếu có nội dung
+                        mainLayout.setVisibility(View.VISIBLE);
                         // Thiết lập sự kiện khi click vào section
-                        view.findViewById(R.id.section_1_main_layout).setOnClickListener(v -> {
+                        mainLayout.setOnClickListener(v -> {
                             // Chuyển dữ liệu category sang Activity tiếp theo
                             SongsListActivity.category = section;
                             startActivity(new Intent(getContext(), SongsListActivity.class));
--- app/src/main/java/vn/nganha/musicapp/Activities/MainActivity.java
+++ app/src/main/java/vn/nganha/musicapp/Activities/MainActivity.java
@@ -1,22 +1,38 @@
 package vn.nganha.musicapp.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.bumptech.glide.request.RequestOptions;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import vn.nganha.musicapp.R;
+import vn.nganha.musicapp.models.SongModel;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RelativeLayout playerView;
+    private TextView songTitleTextView;
+    private ImageView songCoverImageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        playerView = findViewById(R.id.player_view);
+        songTitleTextView = findViewById(R.id.song_title_text_view);
+        songCoverImageView = findViewById(R.id.song_cover_image_view);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationMenu);
 
@@ -52,5 +68,33 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+        // Hiển thị PlayerView nếu cần khi khởi động
+        showPlayerView();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Cập nhật PlayerView khi quay lại Activity
+        showPlayerView();
+    }
+
+    private void showPlayerView() {
+        playerView.setOnClickListener(v -> {
+            startActivity(new Intent(this, PlayerActivity.class));
+        });
+        SongModel currentSong = MyExoplayer.getCurrentSong();
+
+        if (currentSong != null) {
+            playerView.setVisibility(View.VISIBLE);
+            songTitleTextView.setText(currentSong.getTitle());
+
+            Glide.with(this)
+                    .load(currentSong.getCoverUrl())
+                    .apply(RequestOptions.bitmapTransform(new RoundedCorners(32)))
+                    .into(songCoverImageView);
+        } else {
+            playerView.setVisibility(View.GONE);
+        }
     }
 }
